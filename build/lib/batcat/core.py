fb6a8b6fd7d55d2ff1d0762c1f2b634913b558ed
Ewen Wang <wang.enqun@outlook.com>
2021-10-19 11:15:34 +0800

    update to 0.0.4

--- build/lib/batcat/core.py
+++ build/lib/batcat/core.py
@@ -6,12 +6,24 @@ author:     Ewen Wang
 email:      wolfgangwong2012@gmail.com
 license:    Apache License 2.0
 """
-import boto3
 from io import StringIO, BytesIO
-import pandas as pd 
+from datetime import datetime
+import pandas as pd
+import boto3
 
 s3 = boto3.client('s3')
 
+
+def print_event(event):
+    """
+    arg:
+        event: s3 trigger event
+    return:
+        None
+    """
+    import json
+    print("Received event: " + json.dumps(event, indent=2))
+    return None
 
 def get_bucket_key(event):
     """
@@ -24,6 +36,24 @@ def get_bucket_key(event):
     key = event["Records"][0]["s3"]["object"]["key"]
     return bucket, key 
 
+
+def SuccessSignal(bucket, key='.success'):
+    """
+    arg:
+        bucket: target bucket to receive a signal
+        key: signal file
+    return:
+        statue: HTTPS status code
+    """
+    with StringIO() as buffer:
+        buffer.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S %f'))
+        response = s3.put_object(Bucket=bucket, 
+                                 Key=key, 
+                                 Body=buffer.getvalue())
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+    return status
+
+
 def read_csv_from_bucket(bucket, key):
     """
     arg:
@@ -35,7 +65,7 @@ def read_csv_from_bucket(bucket, key):
     df = pd.read_csv(BytesIO(response['Body'].read()))
     return df
 
-def read_excel_from_bucket(bucket, key, sheet_name=0):
+def read_excel_from_bucket(bucket, key, sheet_name=0, header=0):
     """
     arg:
         bucket, key: data source in s3
@@ -44,8 +74,44 @@ def read_excel_from_bucket(bucket, key, sheet_name=0):
         df: raw data, pandas.DataFrame
     """
     response = s3.get_object(Bucket=bucket, Key=key)
-    df = pd.read_excel(BytesIO(response['Body'].read()), sheet_name=sheet_name)
+    df = pd.read_excel(BytesIO(response['Body'].read()), 
+                       sheet_name=sheet_name, 
+                       header=header)
     return df 
+
+def get_data_from_athena(query, 
+                         date_start=None, 
+                         date_end=None,
+                         region,
+                         s3_staging_dir):
+    """
+    arg: 
+        query: querry to obtain data from Athena, str
+        date_start: date to start, strftime('%Y/%m/%d')
+        date_start: date to end, strftime('%Y/%m/%d')
+        region: region of the AWS environment, eg. "cn-northwest-1"
+        s3_staging_dir: s3 staging directory, eg. "s3://#####-###-###-queryresult/ATHENA_QUERY"
+    """
+    from pyathena import connect
+    from pyathena.pandas.cursor import PandasCursor
+    
+    cursor = connect(s3_staging_dir=s3_staging_dir,
+                     region_name=region,
+                     cursor_class=PandasCursor).cursor()
+    
+    query = query.format(date_start, date_end) 
+    df = cursor.execute(query).as_pandas()
+    return df
+
+def get_date_with_delta(delta):
+    """ Get the date delta days ago.
+    arg:
+        delta: the number of days ago, int
+    return:
+        date: strftime('%Y/%m/%d')
+    """
+    from datetime import date, timedelta
+    return (date.today() - timedelta(days=delta)).strftime('%Y/%m/%d')
 
 def save_to_bucket(df, bucket, key):
     """
@@ -62,4 +128,3 @@ def save_to_bucket(df, bucket, key):
                                  Body=csv_buffer.getvalue())
         status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
     return status
-
