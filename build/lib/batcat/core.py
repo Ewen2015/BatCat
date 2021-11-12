5f7740f588c83305c3d3f55d849b4bd68904dfc8
Ewen Wang <wang.enqun@outlook.com>
2021-11-12 17:33:07 +0800

    ignore

--- build/lib/batcat/core.py
+++ build/lib/batcat/core.py
@@ -119,7 +119,7 @@ def get_data_from_athena(query,
     df = cursor.execute(query).as_pandas()
     return df
 
-def get_date_with_delta(delta):
+def get_date_with_delta(delta, format='%Y/%m/%d'):
     """ Get the date delta days ago.
     arg:
         delta: the number of days ago, int
@@ -127,7 +127,7 @@ def get_date_with_delta(delta):
         date: strftime('%Y/%m/%d')
     """
     from datetime import date, timedelta
-    return (date.today() - timedelta(days=delta)).strftime('%Y/%m/%d')
+    return (date.today() - timedelta(days=delta)).strftime(format)
 
 def get_data_from_redshit(query, 
                           date_start, 
@@ -164,3 +164,38 @@ def get_data_from_redshit(query,
     df = cursor.fetch_dataframe()
     return df
 
+def save_df_to_redshift(df,
+                        table_name,
+                        dtype=None,
+                        host=None,
+                        password=None,
+                        port=5439,
+                        database='dev',
+                        user='awsuser',
+                        if_exists='replace'):
+    """
+    arg: 
+        df: target dataframe
+        table_name: target table name'
+        dtype: dict or scalar, optional
+            Specifying the datatype for columns. If a dictionary 
+            is used, the keys should be the column names and the 
+            values should be the SQLAlchemy types or strings for 
+            the sqlite3 legacy mode. If a scalar is provided, it 
+            will be applied to all columns.
+        if_exists: {‘fail’, ‘replace’, ‘append’}, default ‘fail’
+            How to behave if the table already exists.
+            fail: Raise a ValueError.
+            replace: Drop the table before inserting new values.
+            append: Insert new values to the existing table.
+        host: Redshift configuration
+        password: Redshift configuration
+        port: Redshift configuration
+        database: Redshift configuration
+        user: Redshift configuration
+    """
+    from sqlalchemy import create_engine
+    
+    con = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format(USER, PASSWORD, HOST, PORT, DATABASE))
+    df.to_sql(name=table_name, con=con, schema=None, if_exists=if_exists, index=False, index_label=None, chunksize=None, dtype=dtype, method=None)
+    return None
