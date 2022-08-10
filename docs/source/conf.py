d2ceb1b05c0d7c19a11b6ca72e66283afd89ffba
Ewen Wang <wang.enqun@outlook.com>
2022-08-10 16:17:35 +0800

    updated to 0.2.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'BatCat'
 copyright = '2022, Ewen Wang'
 author = 'Ewen Wang'
-release = '0.2.2'
+release = '0.2.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
