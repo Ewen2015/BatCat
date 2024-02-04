a366c371844cea7dee35ffbb5d46d0daf272ea5a
Ewen2015 <wang.enqun@outlook.com>
2024-02-04 15:31:55 +0800

    pypi upgrade to 0.2.49

--- setup.py
+++ setup.py
@@ -8,47 +8,47 @@ from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
-NAME = 'batcat'
-DESCRIPTION = 'BatCat, A Cat Looks Like A Bat.'
-URL = 'https://github.com/Ewen2015/BatCat'
-EMAIL = 'wolfgangwong2012@gmail.com'
-AUTHOR = 'Ewen Wang'
+NAME = "batcat"
+DESCRIPTION = "BatCat, A Cat Looks Like A Bat."
+URL = "https://github.com/Ewen2015/BatCat"
+EMAIL = "wolfgangwong2012@gmail.com"
+AUTHOR = "Ewen Wang"
 REQUIRED = [
-    'pandas', 
-    'boto3==1.18.57',
-    'botocore==1.21.57',
-    'protobuf==3.19.5',
-    'sagemaker==2.59.8', 
-    'stepfunctions==2.2.0',
-    'sagemaker-experiments==0.1.35', 
-    'pyathena==2.3.0', 
-    'redshift_connector==2.0.888', 
-    'sqlalchemy==1.3.23',
-    'psycopg2-binary==2.9.1',
-    'joblib==1.2.0',
-    'awscli', 
-    'requests', 
+    "pandas",
+    "boto3==1.18.57",
+    "botocore==1.21.57",
+    "protobuf==3.19.5",
+    "sagemaker==2.59.8",
+    "stepfunctions==2.2.0",
+    "sagemaker-experiments==0.1.35",
+    "pyathena==2.3.0",
+    "redshift_connector==2.0.888",
+    "sqlalchemy==1.3.23",
+    "psycopg2-binary==2.9.1",
+    "joblib==1.2.0",
+    "requests",
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, NAME, "__version__.py")) as f:
     exec(f.read(), about)
+
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -58,36 +58,37 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
+
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=REQUIRED,
     include_package_data=True,
-    license='Apache License 2.0',
+    license="Apache License 2.0",
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
     ],
     cmdclass={
-        'upload': UploadCommand,
+        "upload": UploadCommand,
     },
 )
